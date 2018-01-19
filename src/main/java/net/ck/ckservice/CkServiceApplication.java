b5f2a4a70301305415ab586fb0de169305d8f9f1
Raj Katipally <rajeshwar.katipally@gmail.com>
2018-01-19 17:31:28 -0500

    Initial commit

---
+++ src/main/java/net/ck/ckservice/CkServiceApplication.java
@@ -0,0 +1,12 @@
+package net.ck.ckservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/net/ck/ckservice/CkServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package net.ck.ckservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CkServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
